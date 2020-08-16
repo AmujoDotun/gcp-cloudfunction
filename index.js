2cf72156560ce307fc4904acba3b9fd277a677ce
AmujoDotun <samuelamujo@gmail.com>
2020-08-16 15:03:08 +0100

    functions for the callback event of the pubsub

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ exports.helloHttp = function helloHttp(req, res)
     res.status(200).send(moment().format('MMM Do YYY, h:mm:ss a'));
 }
 
+exports.psHelloPuSub = function psHelloPuSub(event, callback)
+{
+    console.log(`Hello PubSub ${moment().format('MMM Do YYY, h:mm:ss a')}`);
+    callback();
+}
